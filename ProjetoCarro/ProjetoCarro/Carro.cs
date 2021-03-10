954c4850531f253fd058a61487e5df2ba194f155
Caio <caioboudens00@gmail.com>
2021-03-10 11:59:41 -0300

    Atividade Carro aula

---
+++ ProjetoCarro/ProjetoCarro/Carro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoCarro
+{
+    class Carro
+    {
+        public string cor;
+        public int anoFabricacao;
+        public string modelo;
+        public int velocidadeAtual;
+        public int velocidadeMaxima;
+        public bool seLigado;
+
+        public Carro(string _cor, int _anoFabricacao, string _modelo, int _velocidadeAtual, int _velocidadeMaxima, bool _seLigado)
+        {
+            this.modelo = _modelo;
+            this.cor = _cor;
+            this.anoFabricacao = _anoFabricacao;
+            this.velocidadeAtual = _velocidadeAtual;
+            this.velocidadeMaxima = _velocidadeMaxima;
+            this.seLigado = _seLigado;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Carro Ligado!");
+            seLigado = true;
+            Console.ReadLine();
+
+        }
+
+        public void Acelerar()
+        {
+            velocidadeAtual += 10;
+            Console.WriteLine("Velocidade atual aumentada, a Velocidade agora é de:" + velocidadeAtual);
+            Console.ReadLine();
+        }
+
+        public void Frear()
+        {
+            velocidadeAtual -= 10;
+            Console.WriteLine("Velocidade atual diminuida, a Velocidade agora é de:" + velocidadeAtual);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ProjetoCarro/ProjetoCarro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjetoCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro celta = new Carro("prata", 2012, "2 portas", 0, 300,false);
+
+            celta.Ligar();
+            celta.Acelerar();
+            celta.Acelerar();
+            celta.Frear();
+            celta.Acelerar();
+            
+        }
+    }
+}
