a60707b17d9643148696d30a80c35b5f700d3fe1
Caio <caioboudens00@gmail.com>
2021-03-10 11:36:44 -0300

    Exemplo 2

---
+++ ProjetoNoGit/ProjetoNoGit/Cachorro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoNoGit
+{
+    class Cachorro
+    {
+       public string nome;
+       public string raca;
+       public int idade;
+       public string genero;
+
+       public void AbanarRabo()
+        {
+            Console.WriteLine("Rabo balançando!");
+            Console.ReadLine();
+
+        }
+
+        public void Latir()
+        {
+            Console.WriteLine("Au au");
+            Console.ReadLine();
+        }
+    }
+}
--- ProjetoNoGit/ProjetoNoGit/Program.cs
+++ ProjetoNoGit/ProjetoNoGit/Program.cs
@@ -6,13 +6,26 @@ namespace ProjetoNoGit
     {
         static void Main(string[] args)
         {
+            /*
             Cachorro dog1 = new Cachorro();
             var dog2 = new Cachorro();
             Cachorro dog3 = new Cachorro();
             var dog4 = new Cachorro();
             Cachorro dog5 = new Cachorro();
 
-            Cachorro[] cachorros = {dog1,dog2,dog3,dog4,dog5}
-        }
+            
+            
+           Cachorro[] cachorros = { dog1, dog2, dog3, dog4, dog5 };
+        */
+
+            var bidu = new Cachorro();
+            bidu.nome = "Bidu da Silva";
+            bidu.raca = "Pit-bull";
+            bidu.genero = "Macho";
+            bidu.idade = 26;
+
+            bidu.Latir();
+            bidu.AbanarRabo();
+            }
     }
 }
