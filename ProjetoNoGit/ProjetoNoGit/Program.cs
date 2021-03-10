6c3fa03d55b3e6de4637c118b796d0b1916f4671
Caio <caioboudens00@gmail.com>
2021-03-10 08:49:52 -0300

    Commitado

---
+++ ProjetoNoGit/ProjetoNoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoNoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
