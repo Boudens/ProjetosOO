4ce1805045acff1262f35cae102d13dd14df6a8f
Caio <caioboudens00@gmail.com>
2021-03-10 09:51:19 -0300

    Mudança no texto

--- ProjetoNoGit/ProjetoNoGit/Program.cs
+++ ProjetoNoGit/ProjetoNoGit/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoNoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Poggers!!!!1");
         }
     }
 }
